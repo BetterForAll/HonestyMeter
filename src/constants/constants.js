bc2384d00637ad2cb4d39fc6de01ae445c7b60be
Igor S <befreenow3000@gmail.com>
2023-06-27 21:56:02 +0300

    revert BASE_URL

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://honestymeter.com'
+export const BASE_URL = 'https://honestymeter.com'
 export const PAGE_ABSOLUTE_URL = {
     ABOUT: `${BASE_URL}/about`,
     TERMS: `${BASE_URL}/terms`,
