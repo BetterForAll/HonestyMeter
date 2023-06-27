6dd62f6b3eba6a7ce2fa3b5f504d464feeacc84c
Igor S <befreenow3000@gmail.com>
2023-06-28 01:09:12 +0300

    fix page style - add container

--- src/pages/report/index.js
+++ src/pages/report/index.js
@@ -1,15 +1,33 @@
+import React from 'react'
+import theme from '@/theme';
+import { Box } from '@mui/material'
 import ReportLoading from '@/components/Report/ReportLoading'
 import ReportWrapper from '@/components/Report/ReportWrapper'
-import React from 'react'
 
 
 function ReportPage({ homePageProps = {} }) {
     const { report, closeReport } = homePageProps
 
-    return report ?
-        <ReportWrapper report={report} showArticleInput={closeReport} />
-        :
-        <ReportLoading />
+    return (
+        <Box sx={STYLES.container}>
+
+            {
+                report ?
+
+                    <ReportWrapper report={report} showArticleInput={closeReport} />
+                    :
+                    <ReportLoading />
+            }
+        </Box>
+    )
+}
+
+const STYLES = {
+    container: {
+        width: '100%',
+        margin: '0 auto auto',
+        padding: theme.spacing(2),
+    },
 }
 
 export default ReportPage
