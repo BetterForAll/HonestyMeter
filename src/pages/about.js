b44d7842cfc863e4407e71243e2b4828e8032cf7
Igor S <befreenow3000@gmail.com>
2023-12-23 17:52:46 +0200

    adjust about page text

--- src/pages/about.js
+++ src/pages/about.js
@@ -203,9 +203,10 @@ export default function About() {
           <li><p><b>News Integrity Feed</b> (New Release): Offers analysis of the latest news from leading sources.
             Users can search by keyword or filter by category and country.</p></li>
           <li><p><b>Personal News Integrity Feed for Popular People</b> (New Release): Analyzes the latest news about famous people. Users can search by name</p></li>
+          <li><p><b>Ratings</b> (New Release):  Ratings for the most praised and criticized people.
+            The most objective media sources&apos; ratings will be added in the future.</p></li>
           <li><p><b>Custom Content Analysis</b> (New Release - now with Link Support): Users can submit links or text to receive a comprehensive bias report.
             This feature enables analysis of content not featured on our website and allows authors to reduce bias in their original content.</p></li>
-          <li><p><b>Ratings</b> (To be released soon): Will provide ratings for the most favored and criticized people, and the most objective media sources.</p></li>
         </ul>
 
         <h3>Current State and Updates:</h3>
