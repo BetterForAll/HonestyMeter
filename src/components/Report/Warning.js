ab74772564fcc4348720710ec3ca65d366ad7228
Igor S <befreenow3000@gmail.com>
2024-01-21 15:11:37 +0200

    edit texts

--- src/components/Report/Warning.js
+++ src/components/Report/Warning.js
@@ -5,10 +5,11 @@ import React from 'react'
 
 const TEXTS = {
     title: 'Caution',
-    body: `Due to inherent human biases, it may seem that reports on articles aligning with our views are crafted by opponents.
-     Conversely, those contradicting our beliefs might seem to be authored by allies. However, such perceptions are likely to be incorrect. 
-     These impressions can be caused by the fact that in both scenarios, articles are subjected to critical evaluation. 
-     This report is the product of an AI model that is significantly less biased than human analyses and has been explicitly instructed to strictly maintain 100% neutrality.
+    body: `Due to inherent human biases, it may seem that reports on articles aligning with our views are crafted by opponents. 
+    Conversely, reports about articles that contradict our beliefs might seem to be authored by allies.
+    These impressions can be caused by the fact that in both scenarios, articles are subjected to critical evaluation. 
+    This report is the product of an AI model that is significantly less biased than human analyses
+    and has been explicitly instructed to strictly maintain 100% neutrality.
     `,
     feedbackPart1: `
     Nevertheless, the HonestyMeter is continuously improving through user feedback.
@@ -20,13 +21,26 @@ const TEXTS = {
 
 export default function Warning() {
     return (
-        <Paper sx={{ width: '100%', padding: theme.spacing(1, 3), color: theme.palette.primary.dark, fontSize: theme.typography.fontSize }} >
-            <Typography sx={{ padding: 1, fontSize: 'inherit' }}>
+        <Paper sx={STYLES.paper} >
+            <Typography sx={STYLES.paragraph}>
                 {TEXTS.title}! {TEXTS.body}
             </Typography>
-            <Typography sx={{ padding: 1, fontSize: 'inherit' }}>
+            <Typography sx={STYLES.paragraph}>
                 {TEXTS.feedbackPart1} <Link href="" style={{ textDecoration: 'none' }} >{TEXTS.submitFeedback}</Link> {TEXTS.feedbackPart2}
             </Typography>
         </Paper>
     )
+}
+
+const STYLES = {
+    paper: {
+        width: '100%',
+        padding: theme.spacing(1, 3),
+        color: theme.palette.primary.dark,
+        fontSize: theme.typography.fontSize,
+    },
+    paragraph: {
+        padding: 1,
+        fontSize: 'inherit'
+    }
 }
