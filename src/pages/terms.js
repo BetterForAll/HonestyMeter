52c2e3afe84de14d1b8c824c9b4ab7beb4e16b65
Igor S <befreenow3000@gmail.com>
2024-01-25 10:46:13 +0200

    edit texts

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -5,6 +5,10 @@ import Disclamer from "@/components/Disclamer";
 
 export default function Terms() {
   return <main style={{ maxWidth: '1000px', margin: 'auto', padding: '2rem', fontSize: '1rem' }}>
+
+    <p style={{ textAlign: 'center' }}>
+      By using this website, you agree to the following terms of service, privacy policy, and disclaimer:
+    </p>
     <h2 style={{ textAlign: 'center' }}>
       <a href='/terms-of-service.pdf' target="_blank">
         Terms and Conditions
